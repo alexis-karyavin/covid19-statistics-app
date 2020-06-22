1c27a9f078528dee5f775fb8bae04763446c9e77
alexis-karyavin <alexis-karyavin@yandex.ru>
2020-06-22 22:57:43 +0300

    Add chart

---
+++ src/components/Chart.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="chart-container">
+    <canvas ref="canva"></canvas>
+  </div>
+</template>
+<script>
+  import Chart from 'chart.js'
+  export default {
+    name: "Chart",
+    props: ['options', 'data'],
+    watch: {
+      data: function (newData, oldData) {
+        this.createChart();
+      }
+    },
+    methods: {
+      createChart() {
+        const recoveredData = {
+          label: "Recovered",
+          data: this.getData(this.data.recovered),
+          lineTension: 0.1,
+          fill: false,
+          borderColor: "#bae755",
+        };
+
+        let confirmedData = {
+          label: "Confirmed",
+          data: this.getData(this.data.confirmed),
+          lineTension: 0.1,
+          fill: false,
+          borderColor: "#000000",
+        };
+
+        let chartData = {
+          labels: this.getDateArr(this.data.confirmed),
+          datasets: [recoveredData, confirmedData]
+        };
+
+        let chartOptions = {
+          legend: {
+            display: true,
+            position: 'top',
+            labels: {
+              boxWidth: 80,
+            }
+          }
+        };
+        const chart = new Chart(this.$refs.canva, {
+          type: 'line',
+          data: chartData,
+          options: chartOptions
+        })
+      },
+      update() {
+        const context = this.$refs.canva.getContext('2d');
+        context.clearRect(0, 0, this.$refs.canva.width, this.$refs.canva.height);
+        this.createChart();
+      },
+      getData(data) {
+        let tmp = [];
+        for (let i = data.length - 1; i > 0; i--) {
+          if (i === 0) {
+            break;
+          }
+          tmp.push(data[i].Cases - data[i - 1].Cases);
+        }
+        return tmp;
+      },
+      getDateArr(data) {
+        let arr = [];
+        // for (let i = 0; i < data.length; i++) {
+        //   tmp.push(this.getDateFormatting(data[i].Date));
+        // }
+        arr = data.map(item => this.getDateFormatting(item.Date))
+        return arr;
+      },
+      getDateFormatting(dateStr) {
+        const tmp = new Date(dateStr);
+        const dd = String(tmp.getDate()).padStart(2, '0');
+        const mm = String(tmp.getMonth() + 1).padStart(2, '0'); //January is 0!
+        return dd + '.' + mm;
+      }
+    }
+
+  }
+</script>
+
+<style scoped>
+  .chart-container {
+    margin-top: 1rem;
+  }
+</style>
--- src/views/Country.vue
+++ src/views/Country.vue
@@ -3,26 +3,41 @@
     <v-btn to="/">
       Back
     </v-btn>
-    <h1 class="mb-4 d-flex align-center">
+    <h2 class="mb-4 d-flex align-center">
       {{ data.Country }} <Flag class="ml-4" :code="data.CountryCode" :size="48" />
-    </h1>
+    </h2>
     <InfoCountry :country="data"/>
+    <h3>Confirmed vs Recovered chart</h3>
+    <Chart ref="chart" options="" :data="statistics" />
   </v-container>
 
 </template>
 <script>
   import InfoCountry from "../components/InfoCountry";
   import Flag from "../components/Flag";
+  import Chart from "../components/Chart";
   export default {
     name: "Country",
-    components: {Flag, InfoCountry},
+    components: {Chart, Flag, InfoCountry},
     computed: {
       data() {
         return this.$store.getters.COUNTRY(this.$route.params.id);
+      },
+      statistics() {
+        return this.$store.getters.GET_STATISTICS;
       }
+    },
+    mounted() {
+      this.$store.dispatch('GET_STATISTIC_BEFORE', this.data.Slug);
+    },
+    updated() {
+      // this.$store.dispatch('GET_STATISTIC_BEFORE', this.data.Slug);
+      this.$refs.chart.update();
     }
   }
 </script>
 <style scoped lang="scss">
-
+  h2, h3 {
+    margin: 1rem 0;
+  }
 </style>
